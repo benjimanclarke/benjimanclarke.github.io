e68445db1f968aab448694ff221d489ca9c67eb0
benjimanclarke <ben.clarke15@hotmail.com>
2019-01-24 14:47:40 +0000

    Deletion of JS

--- js/main.js
+++
@@ -1,9 +0,0 @@
-function main() {
-	$('.projectList').hide();
-	$('.projectButton').on('click', function() {
-  $(this).next().slideToggle(400);
-		$(this).next().textToggle('Here they are:');
-	});
-}
-
-$(document).ready(main);
